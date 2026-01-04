0b07d6d3c3356bf5f77b7ad4ae0cd3a34f574d57
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2026-01-04 22:56:20 +0800

    config: integrate Babel loader into webpack

--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
